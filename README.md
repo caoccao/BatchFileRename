ac7c82eb3139b5f8477b763980b1b367fd1b55d2
Sam Cao <sjtucaocao@gmail.com>
2024-09-17 11:15:18 +0800

    ✨ feat: Add 4 tabs

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # BatchFileRename
+
 A batch file rename GUI application with split view for both source and target files.
+
+## License
+
+[APACHE LICENSE, VERSION 2.0](LICENSE)
--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,85 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/tauri";
-import "./App.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import React from "react";
+import Box from "@mui/material/Box";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+  const [tabIndex, setTabIndex] = React.useState(0);
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
-  }
-
+  const onChangeTabIndex = (
+    _event: React.SyntheticEvent,
+    newTabIndex: number
+  ) => {
+    setTabIndex(newTabIndex);
+  };
   return (
-    <div className="container">
-      <h1>Welcome to Tauri!</h1>
-
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
+    <Box sx={{ width: "100%" }}>
+      <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+        <Tabs
+          value={tabIndex}
+          onChange={onChangeTabIndex}
+          aria-label="Batch File Rename"
+        >
+          <Tab
+            label="Unified"
+            id="tab-id-unified"
+            aria-controls="tab-control-unified"
+            sx={{ textTransform: "none" }}
+          />
+          <Tab
+            label="Source"
+            id="tab-id-source"
+            aria-controls="tab-control-source"
+            sx={{ textTransform: "none" }}
+          />
+          <Tab
+            label="Target"
+            id="tab-id-target"
+            aria-controls="tab-control-target"
+            sx={{ textTransform: "none" }}
+          />
+          <Tab
+            label="Settings"
+            id="tab-id-settings"
+            aria-controls="tab-control-settings"
+            sx={{ textTransform: "none" }}
+          />
+        </Tabs>
+      </Box>
+      <div
+        id="tab-id-unified"
+        aria-labelledby="tab-control-unified"
+        hidden={tabIndex !== 0}
       >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-
-      <p>{greetMsg}</p>
-    </div>
+        Unified View
+      </div>
+      <div
+        id="tab-id-source"
+        aria-labelledby="tab-control-source"
+        hidden={tabIndex !== 1}
+      >
+        Source File List
+      </div>
+      <div
+        id="tab-id-target"
+        aria-labelledby="tab-control-target"
+        hidden={tabIndex !== 2}
+      >
+        Target File List
+      </div>
+      <div
+        id="tab-id-settings"
+        aria-labelledby="tab-control-settings"
+        hidden={tabIndex !== 3}
+      >
+        Settings
+      </div>
+    </Box>
   );
 }
 
